512579239a928f932c6b153c0f6d2b32b5dd4246
Rafael Freitas <vidarafael@hotmail.com>
2021-08-26 16:47:32 -0300

    adicionando componente de validação

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext"
 import { AuthTokenError } from "../errors/AuthTokenError";
 import { useCan } from "../hooks/useCan";
@@ -11,10 +12,6 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
-  const userCanSeeMetrics = useCan({
-    roles: ['administrator','editor']
-  })
-
   useEffect(() => {
     api.get('/me')
       .then(response => console.log(response))
@@ -25,7 +22,9 @@ export default function Dashboard() {
     <>
       <h1>Dashboard: {user?.email} </h1>
 
-      { userCanSeeMetrics && <div>Métricas</div> }
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
     </>
   )
 }
