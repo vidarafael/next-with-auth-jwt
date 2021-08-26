c183dfd0ea21205fa4f8b80153d1e6ef0a6b2586
Rafael Freitas <vidarafael@hotmail.com>
2021-08-26 16:38:48 -0300

    adicionando hook de autorização

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,6 +2,7 @@ import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
 import { AuthTokenError } from "../errors/AuthTokenError";
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth"
@@ -10,6 +11,10 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
+  const userCanSeeMetrics = useCan({
+    roles: ['administrator','editor']
+  })
+
   useEffect(() => {
     api.get('/me')
       .then(response => console.log(response))
@@ -17,7 +22,11 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email} </h1>
+    <>
+      <h1>Dashboard: {user?.email} </h1>
+
+      { userCanSeeMetrics && <div>Métricas</div> }
+    </>
   )
 }
 
